9eded326d08387e3d731dbc6da9dc7e3e7ffefa0
axyorah <elvira.bozileva@gmail.com>
2022-02-09 21:29:47 +0100

    mv: moved some env vars from `settings.py` to `.env`; they set in `settings.py` via `python-dotenv`

--- newsnl2en/settings.py
+++ newsnl2en/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+env = dotenv_values('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xv78@6)7$$3je=ak9_8rg+3(pifds+a)^3!$z_30i0$d95++7s'
+SECRET_KEY = env['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(env['DEBUG'])
 
 ALLOWED_HOSTS = []
 
@@ -130,3 +134,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+NEWSAPI_KEY = env['NEWSAPI_KEY']
