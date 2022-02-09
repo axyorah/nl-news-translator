6263f736da1a5d74d755fc63d7689cb3348afca2
axyorah <elvira.bozileva@gmail.com>
2022-02-09 23:08:05 +0100

    mod: adjusted news views to display newsapi query result as json (for now...)

--- news/views.py
+++ news/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+
+import json
+
+from .utils import (
+    NewsRequester,
+    newsform2params
+)
+
+news_requester = NewsRequester(settings.NEWSAPI_KEY)
 
 
 def index(request):
-    print(request)
-    return render(request, 'news/news.html')
+    context = {
+        'categories': [
+            'business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'
+        ]
+    }
+
+    if request.method == 'POST':
+
+        params = newsform2params(request.POST)
+        res = news_requester.get(**params)
+        context['posts'] = json.dumps(res, indent=4)
+
+    return render(request, 'news/news.html', context)
