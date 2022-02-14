6e3c3b86611a69e0dabc580eb1aeb9c086f990bf
axyorah <elvira.bozileva@gmail.com>
2022-02-14 15:42:40 +0100

    mod(display): selected article table looks slightely more decent now

--- static/js/build/article-selected.js
+++ static/js/build/article-selected.js
@@ -5,19 +5,49 @@ export class CustomHTMLTable {
         this.columnSpecs = columnSpecs;
         this.table = this.initTable();
     }
+    getColGroup(columnSpecs) {
+        const colGroup = document.createElement('colgroup');
+        // add first blank column (needed for bootstrap?)
+        const colBlank = document.createElement('col');
+        colBlank.setAttribute('span', '1');
+        colBlank.style.width = '0%';
+        colGroup.appendChild(colBlank);
+        // set column widths
+        columnSpecs.forEach(spec => {
+            const col = document.createElement('col');
+            col.setAttribute('span', '1');
+            col.style.width = `${spec.widthPercent}%`;
+            colGroup.appendChild(col);
+        });
+        return colGroup;
+    }
+    getTHead(columnSpecs) {
+        const thead = document.createElement('thead');
+        const row = document.createElement('tr');
+        const thBlank = document.createElement('th');
+        thBlank.setAttribute('scope', 'col');
+        row.appendChild(thBlank);
+        columnSpecs.forEach(spec => {
+            const th = document.createElement('th');
+            th.setAttribute('scope', 'col');
+            th.innerText = spec.name;
+            row.appendChild(th);
+        });
+        thead.appendChild(row);
+        return thead;
+    }
     initTable() {
         const table = document.createElement('table');
         table.setAttribute('class', 'table');
+        // add column group and table header if column specs are provided
         if (this.columnSpecs) {
-            const colGroup = document.createElement('colgroup');
-            this.columnSpecs.forEach(spec => {
-                const col = document.createElement('col');
-                col.setAttribute('span', '1');
-                col.style.width = `${spec.widthPercent}%`;
-                colGroup.appendChild(col);
-            });
+            const colGroup = this.getColGroup(this.columnSpecs);
             table.appendChild(colGroup);
+            const thead = this.getTHead(this.columnSpecs);
+            table.appendChild(thead);
         }
+        const tbody = document.createElement('tbody');
+        table.appendChild(tbody);
         this.root.appendChild(table);
         return table;
     }
@@ -26,13 +56,17 @@ export class CustomHTMLTable {
             console.assert(columns.length == this.columnSpecs.length, `Number of columns doesn't match the number of column specs: 
                 require ${this.columnSpecs.length} got ${columns.length}`);
         }
+        const tbody = this.table.querySelector('tbody');
         const row = document.createElement('tr');
+        const tdBlank = document.createElement('td');
+        tdBlank.setAttribute('scope', 'row');
+        row.appendChild(tdBlank);
         columns.forEach(colText => {
             const col = document.createElement('td');
             col.innerText = colText;
             row.appendChild(col);
         });
-        this.table.appendChild(row);
+        tbody.appendChild(row);
         return row;
     }
 }
@@ -52,10 +86,10 @@ function translateSentences(sentences) {
 export function displayArticleParagraphs(paragraphsRaw, divRoot) {
     // initialize table
     const columnSpecs = [
-        { 'name': 'original', 'widthPercent': 45 },
-        { 'name': 'original-space', 'widthPercent': 5 },
-        { 'name': 'translation-space', 'widthPercent': 5 },
-        { 'name': 'translation', 'widthPercent': 45 }
+        { 'name': 'Original Text', 'widthPercent': 45 },
+        { 'name': '', 'widthPercent': 5 },
+        { 'name': '', 'widthPercent': 5 },
+        { 'name': 'Translation', 'widthPercent': 45 }
     ];
     const table = new CustomHTMLTable(divRoot, columnSpecs);
     // fill table
