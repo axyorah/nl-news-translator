1e0eb23185dde8c872e2137fb3ed4483bd5c277a
axyorah <elvira.bozileva@gmail.com>
2022-02-15 23:50:01 +0100

    add: added basic registration

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from users import views
 urlpatterns = [
     path('login', views.loginUser, name='login'),
     path('logout', views.logoutUser, name='logout'),
+    path('register', views.registerUser, name='register'),
     path('', views.profile, name='profile'),    
 ]
--- users/views.py
+++ users/views.py
@@ -4,9 +4,11 @@ from django.conf import settings
 
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib import messages
 
+from django.contrib.auth.models import User
 from users.models import Profile
 
 # Create your views here.
@@ -37,13 +39,50 @@ def loginUser(request):
             messages.error(request, 'Wrong Username or Password!')
             return redirect('news')
     
-    context = {}
+    context = {
+        'page': 'login'
+    }
     return render(request, 'users/login_registration.html', context)
 
 def logoutUser(request):
     logout(request)
     messages.info(request, 'Bye!')
     return redirect('news')
+
+def registerUser(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+
+        # check if username in db
+        try:
+            user = User.objects.get(username=username)
+            if user is not None:
+                messages.error(request, f'Username `{username}` already exists!')
+                return redirect('login')
+        finally:
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                # save
+                user = form.save(commit=False)
+                user.username = user.username.lower()
+                user.save()
+
+                # confirm, login, redirect
+                login(request, user)
+                messages.success(request, f'Welcome, {user.username}!')
+                return redirect('profile')
+            else:
+                messages.error(request, 'Invalid data!')
+                return redirect('login')
+    
+    form = UserCreationForm()
+    context = {
+        'page': 'register', 
+        'form': form
+    }
+    return render(request, 'users/login_registration.html', context)
+
 
 @login_required(login_url='login')
 def profile(request):
