f9731c4f60778b6ac681bb8e689f986850b65819
axyorah <elvira.bozileva@gmail.com>
2022-02-15 22:24:33 +0100

    add: added authentication and session at login (django builtin stuff)

--- users/views.py
+++ users/views.py
@@ -2,19 +2,43 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.conf import settings
 
+from django.contrib.auth import login, authenticate
+
 from users.models import Profile
 
 # Create your views here.
 
 def loginUser(request):
-    
+
     if request.method == 'POST':
         username = request.POST['username']
-        profile = Profile.objects.get(username=username)
-        return redirect('profile', pk=profile.id)
+        password = request.POST['password']
+        
+        # check if in db
+        try:
+            profile = Profile.objects.get(username=username)
+        except Exception as e:
+            print(e)
+            print(f'User `{username}` does not exist!')
+            return redirect('news')
+
+        # check if username and password match
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # create session for user and add it to browser cookies
+            login(request, user)
+            profile = Profile.objects.get(username=username)
+            return redirect('profile', pk=profile.id)
+        else:
+            print('username or password is incorrect')
+            # TODO add flash message
+            return redirect('news')
     
     context = {}
     return render(request, 'users/login_registration.html', context)
+
+def logoutUser(request):
+    return redirect('news')
 
 def profile(request, pk):
     profile = Profile.objects.get(id=pk)
