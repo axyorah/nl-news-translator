0af1e8d7f5c8536af40ff0c93ed428d25e856188
axyorah <elvira.bozileva@gmail.com>
2022-02-15 21:43:35 +0100

    add: added signals to properly connect `User` and `Profile` models and registered them with django (now when one is created/deleted so is the other)

---
+++ users/signals.py
@@ -0,0 +1,27 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
+
+from django.contrib.auth.models import User
+from users.models import Profile
+
+@receiver(post_save, sender=User)
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            name=user.username,
+        )
+   
+@receiver(post_delete, sender=Profile)
+def deleteUser(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
+    print(f'User and Profile `{instance}` deleted!')
+
+@receiver(post_save, sender=Profile)
+def profileUpdated(sender, instance, created, **kwargs):
+    # this is for debugging purposes only...
+    print('Profile Saved!')
+    print(f'Sender: {sender}\nInstance: {instance}\nCreated: {created}')
+ 
