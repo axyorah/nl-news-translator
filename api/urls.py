072478665107d5a5725afad8b84d6157a99e6797
axyorah <elvira.bozileva@gmail.com>
2022-02-19 14:27:56 +0100

    add: added CRUD views/urls for tags

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.getRoutes, name='api-routes'),
     path('paragraphs/', views.getParagraphs, name='news-paragraphs'),
     path('translations/', views.getTranslations, name='translations'),
-    path('tags/new/', views.createPartialTag, name='partial-tag-create'),
+    path('tags/new/', views.createTag, name='tag-create'),
+    path('tags/<pk>/edit/', views.updateTag, name='tag-update'),
+    path('tags/<pk>/delete/', views.deleteTag, name='tag-delete'),
+    path('tags/<pk>/', views.getTag, name="tag-get")
 ]
