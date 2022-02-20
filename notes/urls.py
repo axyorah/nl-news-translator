40b3d4768983223b10ef56a98b0bddf446a0209c
axyorah <elvira.bozileva@gmail.com>
2022-02-20 15:26:49 +0100

    mv: moved basic note list from profile page at `/users/` to a separate note page at `/notes/`

--- notes/urls.py
+++ notes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from notes import views
 
 urlpatterns = [
+    path('', views.showNotes, name='note-list'),
     path('new/', views.createNote, name='note-create'),
-]
+]   
--- notes/views.py
+++ notes/views.py
@@ -6,6 +6,19 @@ from django.contrib import messages
 from notes.models import Note, Tag, Membership
 from notes.forms import NoteForm, TagForm
 
+@login_required(login_url='login')
+def showNotes(request):
+    # TODO: paging, filter
+    profile = request.user.profile
+
+    notes = profile.note_set.order_by('created')# filter by 
+
+    context = {
+        'notes': notes
+    }
+
+    return render(request, 'notes/note-list.html', context)
+
 # Create your views here.
 @login_required(login_url='login')
 def createNote(request):
@@ -24,20 +37,13 @@ def createNote(request):
         if noteForm.is_valid():
             note = noteForm.save(commit=False)
             note.owner = profile
-            tags = noteForm.cleaned_data.get('tags')
-            print(tags)
-
-            # tagForm = TagForm(request.POST)
-            # print("TAG.FORM:")
-            # print(tagForm)
-            # if tagForm.is_valid():
-            #     tags = tagForm.save(commit=False)
-            #     tags.owner = profile
-            #     tags.notes = note
-            #     tags.save()
-
             note.save()
             noteForm.save_m2m() # <-- save tags !!!
+
+            #tags = noteForm.cleaned_data.get('tags')
+            #print(tags)
+            #print(note.tag_set.all())
+
             return redirect('profile')
         else:
             print(noteForm)
@@ -61,25 +67,26 @@ def deleteNote(request, pk):
     # TODO: ...
     return redirect('profile')
 
-@login_required(login_url='login')
-def createTag(request, pk):
-    form = TagForm()
-    note = Note.objects.get(id=pk)
+# @login_required(login_url='login')
+# def createTag(request, pk):
+#     # DO IT THROUGH API!
+#     form = TagForm()
+#     note = Note.objects.get(id=pk)
 
-    if request.method == 'POST':
-        form = TagForm(request.POST)
-        if form.is_valid():
-            tag = form.save(commit=False)
-            tag.owner = request.user.profile
-            tag.note = note
-            tag.save()
-        else:
-            messages.error(request, 'Tag data is not valid')
+#     if request.method == 'POST':
+#         form = TagForm(request.POST)
+#         if form.is_valid():
+#             tag = form.save(commit=False)
+#             tag.owner = request.user.profile
+#             tag.note = note
+#             tag.save()
+#         else:
+#             messages.error(request, 'Tag data is not valid')
 
-    context = {
-        'tagForm': form,
-    }
+#     context = {
+#         'tagForm': form,
+#     }
 
-    return render(request, 'notes/note.html', context)
+#     return render(request, 'notes/note.html', context)
     
 
