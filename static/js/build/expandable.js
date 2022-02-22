279d9f4d27c652352ac0179b1fc433eb349190a3
axyorah <elvira.bozileva@gmail.com>
2022-02-22 23:35:21 +0100

    cleanup(minor): cleaned up some unnecessary console logs

--- static/js/build/expandable.js
+++ static/js/build/expandable.js
@@ -1,6 +1,5 @@
 export function toggleExpandable(expandable) {
     expandable.addEventListener('mouseenter', function (evt) {
-        console.log('MOUSEENTER')
         const hiddensElements = this.querySelectorAll('.hidden');
         hiddensElements.forEach((hidden) => {
             hidden.style.display = 'block';
@@ -11,7 +10,6 @@ export function toggleExpandable(expandable) {
         });
     });
     expandable.addEventListener('mouseleave', function (evt) {
-        console.log('MOUSELEAVE')
         const hiddensElements = this.querySelectorAll('.hidden');
         hiddensElements.forEach((hidden) => {
             hidden.style.display = 'none';
