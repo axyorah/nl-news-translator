5790b9c443d403ade7b99ba5c3501cd2e876fc37
axyorah <elvira.bozileva@gmail.com>
2022-04-16 12:52:09 +0200

    mod: wrapped view body in try...except

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.conf import settings
+
+from django.contrib import messages
 
 import json
 
@@ -13,18 +15,24 @@ news_requester = NewsRequester(settings.NEWSAPI_KEY)
 
 
 def index(request):
-    context = {
-        'categories': [
-            'business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'
-        ]
-    }
+    try:
+        context = {
+            'categories': [
+                'business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'
+            ]
+        }
+    
+        if request.method == 'POST':
+    
+            print('news/views.py -> index: request.POST')
+            print(request.POST)
+    
+            params = newsform2params(request.POST)
+            res = news_requester.get(**params)
+            context['articles'] = res.get('articles')
+    
+        return render(request, 'news/news.html', context)
 
-    if request.method == 'POST':
-
-        print(request.POST)
-
-        params = newsform2params(request.POST)
-        res = news_requester.get(**params)
-        context['articles'] = res.get('articles')
-
-    return render(request, 'news/news.html', context)
+    except Exception as e:
+        messages.error(request, e.args[0])
+        return redirect('error')
