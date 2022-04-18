6a2179f40b4a7f081b7f6600369e3204f9279c07
axyorah <elvira.bozileva@gmail.com>
2022-04-18 14:59:35 +0200

    style: styled `SearchBar` component

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 interface SearchBarProps {};
 interface RootState {};
 
 const SearchBar = (props: SearchBarProps) => {
+
+    const [ query, setQuery ] = useState('');
+
+    const onFormSubmit = (evt: React.SyntheticEvent): void => {
+        evt.preventDefault();
+        console.log(`form submitted: q=${query}`);
+    };
+
     return (
-        <div>
-            Search Bar
-        </div>
+        <Container className="boxed mycard p-5" id="box-filter">
+            <h3 className="text-center">Search by Keyword</h3>
+            <Form onSubmit={onFormSubmit}>
+                <Form.Group controlId="query" className="my-2" id="query-container">
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Enter Keyword" 
+                        value={query}
+                        onChange={e => setQuery(e.target.value)}
+                    />
+                </Form.Group>
+
+                <Button variant="secondary" className="w-100 my-2" type="submit">
+                    Search
+                </Button>
+            </Form>
+        </Container>
     );
 };
 
