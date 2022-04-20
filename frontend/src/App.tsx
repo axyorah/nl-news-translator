74dcac6d39a8856894e07f1156409553ad67dcf1
axyorah <elvira.bozileva@gmail.com>
2022-04-20 23:40:38 +0200

    add: added (yet disfunctional) navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Header from './components/Header';
 import NewsScreen from './screens/NewsScreen';
 
 function App() {
   return (
     <Router>
-      App
-
+      <Header />
       <Switch>
         <Container className="App">
           <Route path="/" exact component={NewsScreen} />
