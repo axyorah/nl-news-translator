fe1c40cba4fba265088c38cfe9024447f5c89579
axyorah <elvira.bozileva@gmail.com>
2022-04-21 23:20:05 +0200

    add: added init types and constants for user actions/reducers

---
+++ frontend/src/types/userTypes.ts
@@ -0,0 +1,44 @@
+import {
+    USER_LOGIN_QUERY,
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAIL,
+    USER_LOGOUT,
+} from '../constants/userConstants';
+
+export interface User {
+    id: number,
+    username: string,
+    isAdmin: boolean,
+    token: string
+}
+
+export interface UserInfo {
+    userDetail: User,
+    loading?: string,
+    errors?: string
+}
+
+export interface UserLoginQueryAction {
+    type: typeof USER_LOGIN_QUERY
+}
+
+export interface UserLoginSuccessAction {
+    type: typeof USER_LOGIN_SUCCESS,
+    payload: User
+}
+
+export interface UserLoginFailAction {
+    type: typeof USER_LOGIN_FAIL,
+    payload: string
+}
+
+export interface UserLogoutAction {
+    type: typeof USER_LOGOUT
+}
+
+
+export type UserLoginAction = 
+    UserLoginQueryAction | 
+    UserLoginSuccessAction | 
+    UserLoginFailAction |
+    UserLogoutAction;
