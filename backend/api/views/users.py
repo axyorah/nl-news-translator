fc6a410d0b93b24b226d95bc4cb3f16068772c5b
axyorah <elvira.bozileva@gmail.com>
2022-04-21 20:52:30 +0200

    mod: user profile view is not auth restricted (`rest_framework` provides useful decorators and premission levels)

--- backend/api/views/users.py
+++ backend/api/views/users.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
@@ -7,6 +8,7 @@ from django.contrib.auth.models import User
 from api.serializers import UserSerializer, UserSerializerWithToken
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user # fetched using provided JWT token!
     serializer = UserSerializerWithToken(user, many=False)
