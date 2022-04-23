1740f14be4f592880df6ce65719e05e9510d3cca
axyorah <elvira.bozileva@gmail.com>
2022-04-23 15:00:45 +0200

    add: added models and serializers for Note and Tag

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Note(models.Model):
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    side_a = models.TextField(null=True, blank=True)
+    side_b = models.TextField(null=True, blank=True)
+
+    tags = models.ManyToManyField('Tag', blank=True) 
+
+    def json(self, tag=False):
+        return {
+            'id': str(self.id),
+            'created': self.created.isoformat(),
+            'owner': str(self.owner.id),
+            'side_a': str(self.side_a),
+            'side_b': str(self.side_b),
+            'tags': [
+                tagObj.json() if tag else tagObj.id
+                for tagObj in self.tags.all()
+            ] if self.tags else []
+        }
+
+    def __str__(self):
+        return f'{self.side_a[:50] if self.side_a else ""}... -> {self.side_b[:50] if self.side_b else ""}...'
+
+class Tag(models.Model):
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+    created = models.DateField(auto_now_add=True)
+
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def json(self, note=False):
+        return {
+            'id': str(self.id),
+            'created': self.created.isoformat(),
+            'name': str(self.name),
+            'owner': str(self.owner.id),
+            'notes': [
+                noteObj.json() if note else noteObj.id
+                for noteObj in self.notes.all()
+            ] if self.notes else []
+        }
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        unique_together = [['owner', 'name']]
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,6 +4,8 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 from django.contrib.auth.models import User
 
+from backend.api.models import Note, Tag
+
 
 class UserSerializer(serializers.ModelSerializer):
     isAdmin = serializers.SerializerMethodField(read_only=True)
@@ -34,3 +36,33 @@ class UserSerializerWithToken(UserSerializer):
     def get_token(self, obj):
         token = RefreshToken.for_user(obj)
         return str(token.access_token)
+
+class NoteSerializer(serializers.ModelSerializer):
+    tags = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Note
+        fields = [
+            'id',
+            'created',
+            'owner',
+            'side_a',
+            'side_b',
+            'tags'
+        ]
+
+    def get_created(self, obj: Note):
+        return obj.created.isoformat()
+
+    def get_tags(self, obj: Note):
+        return obj.tags.all() or []
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = [
+            'id',
+            'created',
+            'name',
+            'owner'
+        ]
