b5a9ca6ff4cb5e8d6df149b37b8bb7519223b525
axyorah <elvira.bozileva@gmail.com>
2022-04-23 02:04:04 +0200

    add: added logout option to header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-const Header = (): JSX.Element => {
+import { StoreState } from '../types/storeTypes';
+import { UserLoginInfo } from '../types/userTypes';
+
+import { logout } from '../actions/userActions';
+
+
+interface HeaderState {
+    userLoginInfo: UserLoginInfo
+}
+
+interface HeaderDispatch {
+    logout: Function
+}
+
+
+const Header = (props: HeaderState & HeaderDispatch): JSX.Element => {
+
+    console.log(props)
+
+    const { userLoginInfo, logout } = props;
+    const { userDetail } = userLoginInfo || {};
+
+    useEffect(() => {
+        console.log(userDetail)
+    }, [ userLoginInfo ]);
+
+    const logoutHandler = () => {
+        logout()
+    };
+
+
     return (
         <Navbar bg="dark" variant="dark" expand="md">
             <Container>
                 <Navbar.Brand as={Link} to="/">HOME</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">                        
-                        <Nav.Link as={Link} to="/users">My Account</Nav.Link>
-                        <Nav.Link as={Link} to="/notes">My Notes</Nav.Link>
-                        <Nav.Link as={Link} to="/tags">My Tags</Nav.Link>
-                    </Nav>
-                    <Nav className="ms-auto">                        
-                        <Nav.Link as={Link} to="/login">Login/Sign Up</Nav.Link>
-                    </Nav>
+
+                    { userDetail && userDetail.username 
+                        ? <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/notes">My Notes</Nav.Link>
+                            <Nav.Link as={Link} to="/tags">My Tags</Nav.Link>
+                        </Nav>
+                        : null
+                    }
+
+                    { userDetail && userDetail.username 
+                        ? <Nav className="ms-auto">
+                            <Nav.Link as={Link} to="/profile">{userDetail.username}</Nav.Link>
+                            <Nav.Link as={Link} to="#" onClick={logoutHandler}>Logout</Nav.Link>
+                        </Nav>
+                        : <Nav className="ms-auto">
+                            <Nav.Link as={Link} to="/login">Login/Sign Up</Nav.Link>
+                        </Nav>
+                    }
+
                 </Navbar.Collapse>
             </Container>
         </Navbar>
     );
 };
 
-export default Header;
+const mapStateToProps = (state: StoreState): HeaderState => {
+    return {
+        userLoginInfo: state.userLoginInfo
+    }
+};
+
+export default connect(
+    mapStateToProps,
+    { logout }
+)(Header);
