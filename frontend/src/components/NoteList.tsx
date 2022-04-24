734a622cfbffdfbc339a09a6ead67636436e182f
axyorah <elvira.bozileva@gmail.com>
2022-04-24 23:38:19 +0200

    refactor: rendering of note list/table is now done through a separate component

---
+++ frontend/src/components/NoteList.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { ListGroup, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import { Note } from '../types/noteTypes';
+import { Tag } from '../types/tagTypes';
+
+
+interface NoteListProps {
+    noteList: Note[]
+}
+
+const NoteList = (props: NoteListProps) => {
+
+    const { noteList } = props;
+ 
+    const renderNoteHeader = (): JSX.Element => {
+        return (
+            <ListGroup.Item style={{ backgroundColor: 'rgb(50, 50, 55)'}}>
+                <Row>
+                    <Col md={2} sm={12}><h5>Date</h5></Col>
+                    <Col md={3} sm={6}><h5>Side A</h5></Col>
+                    <Col md={3} sm={6}><h5>Side B</h5></Col>
+                    <Col md={4} sm={12}><h5>Tags</h5></Col>
+                </Row>
+            </ListGroup.Item>
+        );
+    };
+
+    const renderNote = (note: Note): JSX.Element => {
+    
+        return (
+            <Row
+                onMouseEnter={e => {
+                    e.currentTarget.children[1].children[0].toggleAttribute('hidden')
+                    e.currentTarget.children[1].children[1].toggleAttribute('hidden')
+                    e.currentTarget.children[2].children[0].toggleAttribute('hidden')
+                }}
+                onMouseLeave={e => {
+                    e.currentTarget.children[1].children[0].toggleAttribute('hidden')
+                    e.currentTarget.children[1].children[1].toggleAttribute('hidden')
+                    e.currentTarget.children[2].children[0].toggleAttribute('hidden')
+                }}
+            >   
+                <Col md={2} sm={12} className='note-created'>
+                    <Link to={`/notes/${note.id}`}>{ note.created.split('T')[0] }</Link>
+                </Col>
+    
+                <Col md={3} sm={6} className='note-side-a'>
+                    <div>
+                        { note.side_a.slice(0, 50) }
+                        { note.side_a.length > 50 ? '...' : null }
+                    </div>
+                    <div hidden>
+                        <pre><code>{note.side_a}</code></pre>
+                    </div>
+                </Col>
+    
+                <Col md={3} sm={6} className='note-side-b'>
+                    <div hidden>
+                        <pre><code>{note.side_b}</code></pre>
+                    </div>
+                </Col>
+    
+                <Col md={4} sm={12} className='note-tags'>
+                    {note.tags.map((tag: Tag) => {
+                        return (
+                            <div 
+                                key={tag.name}
+                                className='capsule' 
+                                style={{ width: 'fit-content' }}
+                            >
+                                {tag.name}
+                            </div>
+                        );
+                    })}
+                </Col>
+            </Row>
+        );
+    };
+
+    return (
+        <ListGroup variant='flush'>
+            { renderNoteHeader()}
+            { noteList.map((note: Note) => {
+                return (<ListGroup.Item key={note.id}>
+                    {renderNote(note)}
+                </ListGroup.Item>);
+            }) }
+        </ListGroup>
+    );
+};
+
+export default NoteList;
--- frontend/src/screens/NoteListScreen.tsx
+++ frontend/src/screens/NoteListScreen.tsx
@@ -1,16 +1,14 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Row, Col, ListGroup } from 'react-bootstrap';
 import { RouteComponentProps } from 'react-router';
-import { Link } from 'react-router-dom';
 
 import { StoreState } from '../types/storeTypes';
-import { Tag } from '../types/tagTypes';
-import { Note, NoteListInfo } from '../types/noteTypes';
+import { NoteListInfo } from '../types/noteTypes';
 
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Paginator from '../components/Paginator';
+import NoteList from '../components/NoteList';
 
 import { getAllUserNotes } from '../actions/noteActions';
 
@@ -41,101 +39,24 @@ const NoteListScreen = (
 
     }, [ getAllUserNotes, location.search ]);
 
-    const renderNote = (note: Note): JSX.Element => {
-        
-        return (
-            <Row
-                onMouseEnter={e => {
-                    e.currentTarget.children[1].children[0].toggleAttribute('hidden')
-                    e.currentTarget.children[1].children[1].toggleAttribute('hidden')
-                    e.currentTarget.children[2].children[0].toggleAttribute('hidden')
-                }}
-                onMouseLeave={e => {
-                    e.currentTarget.children[1].children[0].toggleAttribute('hidden')
-                    e.currentTarget.children[1].children[1].toggleAttribute('hidden')
-                    e.currentTarget.children[2].children[0].toggleAttribute('hidden')
-                }}
-            >   
-                <Col md={2} sm={12}>
-                    <Link to={`/notes/${note.id}`}>{ note.created.split('T')[0] }</Link>
-                </Col>
-
-                <Col md={3} sm={6}>
-                    <div>
-                        { note.side_a.slice(0, 50) }
-                        { note.side_a.length > 50 ? '...' : null }
-                    </div>
-                    <div hidden>
-                        <pre><code>{note.side_a}</code></pre>
-                    </div>
-                </Col>
-
-                <Col md={3} sm={6}>
-                    <div hidden>
-                        <pre><code>{note.side_b}</code></pre>
-                    </div>
-                </Col>
-
-                <Col md={3} sm={12}>
-                    {note.tags.map((tag: Tag) => {
-                        return (
-                            <div 
-                                key={tag.name}
-                                className='capsule' 
-                                style={{ width: 'fit-content' }}
-                            >
-                                {tag.name}
-                            </div>
-                        );
-                    })}
-                </Col>
-            </Row>
-        );
-    };
-
-    const renderNotes = (): JSX.Element | null => {
-        if (!noteList) {
-            return null;
-        }
-
-        return (
-            <ListGroup variant='flush'>
-                
-                <ListGroup.Item style={{ backgroundColor: 'rgb(50, 50, 55)'}}>
-                    <Row>
-                        <Col md={2} sm={12}><h5>Date</h5></Col>
-                        <Col md={3} sm={6}><h5>Side A</h5></Col>
-                        <Col md={3} sm={6}><h5>Side B</h5></Col>
-                        <Col md={3} sm={12}><h5>Tags</h5></Col>
-                    </Row>
-                </ListGroup.Item>
-
-                { noteList.map((note: Note) => {
-                    return (<ListGroup.Item key={note.id}>
-                        {renderNote(note)}
-                    </ListGroup.Item>);
-                }) }
-            </ListGroup>
-        );
-    };
-
     return (
         <div className='boxed mycard mt-5 p-5'>
+
             <h3 className='text-center mb-4'>My Notes</h3>
 
             { loading ? <Loader /> : null }
             { errors ? <Message variant='danger'>{errors}</Message> : null }
-            { noteListDetail 
-                ? <div>
-                    { renderNotes() }
+            { noteListDetail
+                ? (<div>
+                    <NoteList noteList={noteList} />
                     <Paginator 
                         baseURL='/notes/' 
                         params={{page: page}} 
                         page={page} 
                         numPages={numPages}
                     />
-                </div> 
-                : null 
+                </div>)
+                : null
             }
 
         </div>
