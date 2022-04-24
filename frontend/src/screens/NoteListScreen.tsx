1d3ce8f2a9710e1df8f990e19825941ae232d976
axyorah <elvira.bozileva@gmail.com>
2022-04-25 01:06:02 +0200

    add: added a first take on note filter by tags - it's currently disfunctional

--- frontend/src/screens/NoteListScreen.tsx
+++ frontend/src/screens/NoteListScreen.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Form, Row, Col } from 'react-bootstrap';
 import { RouteComponentProps } from 'react-router';
 
 import { StoreState } from '../types/storeTypes';
+import { Tag, TagListInfo } from '../types/tagTypes';
 import { NoteListInfo } from '../types/noteTypes';
 
 import Loader from '../components/Loader';
@@ -11,14 +13,21 @@ import Paginator from '../components/Paginator';
 import NoteList from '../components/NoteList';
 
 import { getAllUserNotes } from '../actions/noteActions';
+import { getAllUserTags } from '../actions/tagActions';
 
 
 interface NoteListScreenState {
-    noteListInfo: NoteListInfo
+    noteListInfo: NoteListInfo,
+    tagListInfo: TagListInfo
 }
 
 interface NoteListScreenDispatch {
-    getAllUserNotes: Function
+    getAllUserNotes: Function,
+    getAllUserTags: Function
+}
+
+interface TagState {
+    [name: string]: boolean
 }
 
 
@@ -26,9 +35,18 @@ const NoteListScreen = (
     props: RouteComponentProps & NoteListScreenState & NoteListScreenDispatch
 ): JSX.Element => {
 
-    const { location, noteListInfo, getAllUserNotes } = props;
-    const { loading, errors, noteListDetail } = noteListInfo || {};
+    const { 
+        location, 
+        noteListInfo, tagListInfo,
+        getAllUserNotes, getAllUserTags 
+    } = props;
+
+    const { loading: loadingNotes, errors: errorsNotes, noteListDetail } = noteListInfo || {};
     const { noteList, page, numPages } = noteListDetail || {};
+
+    const { loading: loadingTags, errors: errorsTags, tagList } = tagListInfo || {};    
+
+    const [ tags, setTags ] = useState<TagState>({});
 
     useEffect(() => {
         const pagePattern = /page=(?<page>[0-9]*)/;
@@ -36,16 +54,75 @@ const NoteListScreen = (
         const page = pageRgx && pageRgx.groups ? pageRgx.groups.page : 1;
 
         getAllUserNotes(page);
+        getAllUserTags();
 
-    }, [ getAllUserNotes, location.search ]);
+    }, [ getAllUserNotes, getAllUserTags, location.search ]);
+
+    useEffect(() => {
+        const tagIds = Object.keys(tags).filter(
+            (tagId: string) => tags[tagId]
+        );
+        console.log(tags);
+        console.log(tagIds);
+        
+    }, [ tags ]);
+
+    const renderTags = () => {
+        return (
+            <div className='m-3'>
+                <Row>
+                    { tagList.map((tag: Tag) => {
+                        return (
+                            <Col 
+                                key={tag.name}
+                                className='capsule' 
+                                style={{ display: 'flex', flexDirection: 'row' }}                                
+                            >
+                                <Form.Check 
+                                    className='m-0' 
+                                    onClick={e => {
+                                        setTags({
+                                            ...tags,
+                                            [tag.id]: tags[tag.id] ? false : true 
+                                        });
+                                    }}
+                                />
+                                <Form.Label className='m-0' >{tag.name}</Form.Label>
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </div>
+        );
+        // return (
+        //     <div className='m-3'>
+        //         <Form as={Row}>
+        //             { tagList.map((tag: Tag) => {
+        //                 return (
+        //                     <Form.Group as={Col} className='capsule' style={{ display: 'flex', flexDirection: 'row' }}>
+        //                         <Form.Check className='m-0' />
+        //                         <Form.Label className='m-0' >{tag.name}</Form.Label>
+        //                     </Form.Group>
+        //                 );
+        //             })}
+        //         </Form>
+        //     </div>
+        // );
+    };
 
     return (
         <div className='boxed mycard mt-5 p-5'>
 
             <h3 className='text-center mb-4'>My Notes</h3>
 
-            { loading ? <Loader /> : null }
-            { errors ? <Message variant='danger'>{errors}</Message> : null }
+            { loadingNotes || loadingTags ? <Loader /> : null }
+            { errorsNotes || errorsTags 
+                ? <Message variant='danger'>{errorsNotes || errorsTags}</Message> 
+                : null 
+            }
+
+            { renderTags() }
+
             { noteListDetail
                 ? (<div>
                     <NoteList noteList={noteList} />
@@ -65,11 +142,12 @@ const NoteListScreen = (
 
 const mapStateToProps = (state: StoreState): NoteListScreenState => {
     return {
-        noteListInfo: state.noteListInfo
+        noteListInfo: state.noteListInfo,
+        tagListInfo: state.tagListInfo
     }
 };
 
 export default connect<NoteListScreenState, NoteListScreenDispatch, {}, StoreState>(
     mapStateToProps,
-    { getAllUserNotes }
+    { getAllUserNotes, getAllUserTags }
 )(NoteListScreen);
