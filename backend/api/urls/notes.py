5dd295b242882664576c5a3e74d93b82b31fdde4
axyorah <elvira.bozileva@gmail.com>
2022-04-25 17:55:31 +0200

    add: added route at `/api/notes/<id>/` for displaying selected user note (visible only for author)

--- backend/api/urls/notes.py
+++ backend/api/urls/notes.py
@@ -5,4 +5,5 @@ from api.views import notes as views
 
 urlpatterns = [
     path('', views.getAllUserNotes, name='all-user-notes'),
+    path('<str:pk>/', views.getUserNote, name='user-note'),
 ]
--- backend/api/views/notes.py
+++ backend/api/views/notes.py
@@ -1,9 +1,11 @@
+from urllib.error import HTTPError
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
 from django.core.paginator import Paginator
+from rest_framework import exceptions
 
 from api.models import Note
 from api.serializers import NoteSerializer
@@ -44,8 +46,32 @@ def getAllUserNotes(request):
             'page': page,
             'num_pages': paginator.num_pages
         })
+
+    except exceptions.APIException as e:
+        return Response({ 'errors': e.detail }, status=e.status_code)
     
     except Exception as e:
         print(e)
         return Response({ 'errors': e.args[0] }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)        
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserNote(request, pk):
+    try:
+        user = request.user
+        note = user.note_set.get(id=pk)
+        serializer = NoteSerializer(note, many=False)
+
+        return Response(serializer.data)
+
+    except Note.DoesNotExist as e:
+        return Response({ 'errors': e.args[0] }, status=status.HTTP_404_NOT_FOUND)
+
+    except exceptions.APIException as e:
+        return Response({ 'errors': e.detail }, status=e.status_code)
+
+    except Exception as e:
+        print(e)
+        return Response({ 'errors': e.args[0] }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
