7a42564bd6d3efcb03dca29b64c5c13ea23cf8dd
axyorah <elvira.bozileva@gmail.com>
2022-04-25 20:06:07 +0200

    add: added Note and Tag forms

---
+++ backend/api/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+
+from django.forms import ModelForm
+from api.models import Note, Tag
+
+class NoteForm(ModelForm):
+    class Meta:
+        model = Note
+        fields = [
+            'side_a',
+            'side_b',
+            'tags'
+        ]
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+        labels = {
+            'side_a': 'Side A',
+            'side_b': 'Side B'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(NoteForm, self).__init__(*args, **kwargs)
+
+        self.fields['side_a'].widget.attrs.update({'class': 'boxed'})
+        self.fields['side_b'].widget.attrs.update({'class': 'boxed'})
+
+
+class TagForm(ModelForm):
+    class Meta:
+        model = Tag
+        fields = [
+            'name'
+        ]
