5eafdfb4640934fab625ed01ef23a26b30dbd2c4
axyorah <elvira.bozileva@gmail.com>
2022-04-25 22:33:30 +0200

    add: adde api route for updating notes at `/api/notes/<id>/edit/`

--- backend/api/urls/notes.py
+++ backend/api/urls/notes.py
@@ -5,5 +5,6 @@ from api.views import notes as views
 
 urlpatterns = [
     path('', views.getAllUserNotes, name='all-user-notes'),
-    path('<str:pk>/', views.getUserNote, name='user-note'),
+    path('<str:pk>/', views.getUserNote, name='get-user-note'),
+    path('<str:pk>/edit/', views.updateUserNote, name='update-user-note'),
 ]
--- backend/api/views/notes.py
+++ backend/api/views/notes.py
@@ -1,4 +1,7 @@
 from urllib.error import HTTPError
+from typing import List, Dict, Optional, Union
+from django.forms import ValidationError
+from django.http import HttpRequest
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
@@ -7,8 +10,10 @@ from django.conf import settings
 from django.core.paginator import Paginator
 from rest_framework import exceptions
 
-from api.models import Note
-from api.serializers import NoteSerializer
+from api.models import Note, Tag
+from api.serializers import NoteSerializer, TagSerializer
+from api.forms import NoteForm, TagForm
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -69,6 +74,49 @@ def getUserNote(request, pk):
         return Response({ 'errors': e.args[0] }, status=status.HTTP_404_NOT_FOUND)
 
     except exceptions.APIException as e:
+        return Response({ 'errors': e.detail }, status=e.status_code)
+
+    except Exception as e:
+        print(e)
+        return Response({ 'errors': e.args[0] }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserNote(request: HttpRequest, pk):
+    try:
+        user = request.user
+        note = user.note_set.get(id=pk)
+        note_orig = user.note_set.get(id=pk)
+
+        # make sure that we're updating existing note instead of creating new
+        noteForm = NoteForm(request.data, instance=note_orig)
+
+        # fugly constraint on tags visible for `this` note:
+        # this constraint should be added during model creation via `Membership`,
+        # but that creates errors for some reason...
+        noteForm.fields['tags'].queryset = user.tag_set.all()
+
+        if noteForm.is_valid():
+            # update form fields
+            note = noteForm.save(commit=False)
+
+            # update m2m
+            note.save()
+            noteForm.save_m2m()
+
+        else:
+            raise Exception('Note update failed')
+
+        serializer = NoteSerializer(note, many=False)
+        return Response(serializer.data)
+    
+    except Note.DoesNotExist as e:
+        print(e)
+        return Response({ 'errors': e.args[0] }, status=status.HTTP_404_NOT_FOUND)
+
+    except exceptions.APIException as e:
+        print(e)
         return Response({ 'errors': e.detail }, status=e.status_code)
 
     except Exception as e:
