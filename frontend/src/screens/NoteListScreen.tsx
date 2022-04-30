de4542c05786bd722bcf0215ab1a8afa069d57dd
axyorah <elvira.bozileva@gmail.com>
2022-04-30 20:42:08 +0200

    mod: `Add Note` button now triggers a popup instead of redirect

--- frontend/src/screens/NoteListScreen.tsx
+++ frontend/src/screens/NoteListScreen.tsx
@@ -1,29 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { Form, Row, Col } from 'react-bootstrap';
+import { Form, Row, Col, Modal, Button } from 'react-bootstrap';
 import { RouteComponentProps } from 'react-router';
 
 import { StoreState } from '../types/storeTypes';
 import { Tag, TagListInfo } from '../types/tagTypes';
-import { NoteListInfo } from '../types/noteTypes';
+import { NoteListInfo, NoteCreateInfo } from '../types/noteTypes';
 
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Paginator from '../components/Paginator';
 import NoteList from '../components/NoteList';
+import NoteForm from '../components/NoteForm';
 
-import { getAllUserNotes } from '../actions/noteActions';
+import { getAllUserNotes, createUserNote, resetCreateUserNote } from '../actions/noteActions';
 import { getAllUserTags } from '../actions/tagActions';
 
 
 interface NoteListScreenState {
     noteListInfo: NoteListInfo,
+    noteCreateInfo: NoteCreateInfo,
     tagListInfo: TagListInfo
 }
 
 interface NoteListScreenDispatch {
     getAllUserNotes: Function,
+    createUserNote: Function,
+    resetCreateUserNote: Function,
     getAllUserTags: Function
 }
 
@@ -38,16 +41,19 @@ const NoteListScreen = (
 
     const { 
         location, history,
-        noteListInfo, tagListInfo,
-        getAllUserNotes, getAllUserTags 
+        noteListInfo, noteCreateInfo, tagListInfo,
+        getAllUserNotes, createUserNote, resetCreateUserNote, 
+        getAllUserTags 
     } = props;
 
     const { loading: loadingNotes, errors: errorsNotes, noteListDetail } = noteListInfo || {};
     const { noteList, page, numPages } = noteListDetail || {};
 
+    const { loading: loadingNoteCreate, errors: errorsNoteCreate, noteCreate } = noteCreateInfo || {};
     const { loading: loadingTags, errors: errorsTags, tagList } = tagListInfo || {};    
 
     const [ tags, setTags ] = useState<TagState>({});
+    const [ hidePopup, setHidePopup ] = useState<boolean>(true);
 
     // on load
     useEffect(() => {
@@ -75,6 +81,14 @@ const NoteListScreen = (
         getAllUserNotes(page, tagIds);
 
     }, [ getAllUserNotes, getAllUserTags, location.search, tags ]);
+
+    // on successful note create
+    useEffect(() => {
+        if (noteCreate) {
+            resetCreateUserNote();
+            setHidePopup(true);
+        }
+    }, [ noteCreate, resetCreateUserNote ]);
 
 
     const renderTag = (tag: Tag): JSX.Element => {
@@ -110,10 +124,30 @@ const NoteListScreen = (
     const renderButtons = (): JSX.Element => {
         return (
             <div className='w-100 d-flex justify-content-end'>
-                <Link to='/notes/new' className='capsule-lg'>
+                <Button 
+                    className='capsule-lg'
+                    onClick={e => setHidePopup(!hidePopup)}
+                >
                     Add Note
-                </Link>
+                </Button>
             </div>
+        );
+    };
+
+    const renderPopup = () => {
+        return (
+            <Modal
+                size='lg'
+                show={!hidePopup}
+                onHide={() => setHidePopup(true)}
+            >
+                <Modal.Header closeButton className='mycard'>
+                    <h3 className='px-3'>Add New Note</h3>
+                </Modal.Header>
+                <Modal.Body className='mycard'>
+                    <NoteForm updateOrCreateNote={createUserNote} />
+                </Modal.Body>
+            </Modal>
         );
     };
 
@@ -122,8 +156,13 @@ const NoteListScreen = (
 
             <h3 className='text-center mb-4'>My Notes</h3>
 
-            { loadingTags ? <Loader /> : null }
-            { errorsTags ? <Message variant='danger'>{errorsTags}</Message> : null }
+            { loadingTags || loadingNoteCreate ? <Loader /> : null }
+            { errorsTags || errorsNoteCreate 
+                ? <Message variant='danger'>
+                    { errorsTags || errorsNoteCreate }
+                </Message> 
+                : null 
+            }
 
             { tagList ? renderTags() : null }
 
@@ -145,6 +184,8 @@ const NoteListScreen = (
 
             { renderButtons() }
 
+            { renderPopup() }
+
         </div>
     );
 };
@@ -152,11 +193,12 @@ const NoteListScreen = (
 const mapStateToProps = (state: StoreState): NoteListScreenState => {
     return {
         noteListInfo: state.noteListInfo,
+        noteCreateInfo: state.noteCreateInfo,
         tagListInfo: state.tagListInfo
     }
 };
 
 export default connect<NoteListScreenState, NoteListScreenDispatch, {}, StoreState>(
     mapStateToProps,
-    { getAllUserNotes, getAllUserTags }
+    { getAllUserNotes, createUserNote, resetCreateUserNote, getAllUserTags }
 )(NoteListScreen);
