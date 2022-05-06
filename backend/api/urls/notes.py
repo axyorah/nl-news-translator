93da6d0c0ec5e42148a09e344333c3de4491082a
axyorah <elvira.bozileva@gmail.com>
2022-05-06 13:49:54 +0200

    mod: added names to note urlpatterns

--- backend/api/urls/notes.py
+++ backend/api/urls/notes.py
@@ -4,6 +4,6 @@ from api.views import notes as views
 
 
 urlpatterns = [
-    path('', views.NoteList.as_view()),
-    path('<str:pk>/', views.NoteDetail.as_view())
+    path('', views.NoteList.as_view(), name='note-list'),
+    path('<str:pk>/', views.NoteDetail.as_view(), name='note-detail')
 ]
