3c666c343524a8100a1920206e9ed7a3612934fe
axyorah <elvira.bozileva@gmail.com>
2022-05-08 15:21:26 +0200

    refactor: factored the error handling bits out of news views

--- backend/api/views/news.py
+++ backend/api/views/news.py
@@ -15,100 +15,79 @@ nl2en = NlToEnTranslator()
 
 def validate_news_query(params):
     #TODO: proper param validation
-    return 
+    return
+
+def try_except(view):
+    def helper(*args, **kwargs):
+        try:
+            return view(*args, **kwargs)
+
+        except exceptions.APIException as e:
+            print(e)
+            return Response({ 'errors': e.detail, 'detail': e.detail }, status=e.status_code)
+
+        except AttributeError as e:
+            print(e)
+            return Response({'errors': e.args[0], 'detail': e.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+
+        except AssertionError as e:
+            print(e)
+            return Response({'errors': e.args[0], 'detail': e.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            print(e)
+            return Response({ 'errors': e.args[0], 'detail': e.args[0] }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return helper
 
 @api_view(['GET'])
+@try_except
 def getAllNews(request: HttpRequest):
 
-    try:
-        q = request.GET.get('q', None)
-        categories = request.GET.get('category_list', None)
-        
-        params = {
-            'q': q,
-            'category_list': categories.split(',') if categories is not None else None
-        }
-
-        validate_news_query(params)
-        res = news_requester.get(**params)
-        
-        return Response(res)
-
-    except exceptions.APIException as e:
-            print(e)
-            return Response(
-                { 'errors': e.detail }, 
-                status=e.status_code
-            )    
-
-    except Exception as e:
-        print(e)
-        # (setting status code to default 200, so that that the 
-        # custom error msg can be properly intercepted in frontend)
-        return Response({'errors': e.args[0]})#, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    q = request.GET.get('q', None)
+    categories = request.GET.get('category_list', None)
+    
+    params = {
+        'q': q,
+        'category_list': categories.split(',') if categories is not None else None
+    }
+    validate_news_query(params)
+    res = news_requester.get(**params)
+    
+    return Response(res)
 
 @api_view(['GET'])
+@try_except
 def getSelectedNews(request: HttpRequest):
+    source = request.GET.get('source', None)
+    url = request.GET.get('url', None)
 
-    try:
-        source = request.GET.get('source', None)
-        url = request.GET.get('url', None)
-        
-        if source is None or url is None:
-            raise Exception(
-                f'Please specify both `url` and `source` in a query string, e.g., '
-                f'`/api/news/?source=nos.nl&url=<url>`'
-            )
-            
-        source = source.lower()        
-        
-        if (source not in source2paragraphs):
-            raise Exception(
-                f'Posts from `{source}` cannot be parsed yet. '
-                f'Please choose one of: {", ".join(list(source2paragraphs.keys()))}'
-            )
-
-        paragraphs = source2paragraphs[source](url)        
-        return Response({ 'paragraphs': paragraphs })
-
-    except exceptions.APIException as e:
-        print(e)
-        return Response(
-            { 'errors': e.detail }, 
-            status=e.status_code
+    if source is None or url is None:
+        raise AttributeError(
+            f'Please specify both `url` and `source` in a query string, e.g., '
+            f'`/api/news/?source=nos.nl&url=<url>`'
         )
 
-    except Exception as e:
-        print(e)
-        # (setting it to default 200, so that that the 
-        # custom error msg can be properly intercepted in frontend)
-        return Response({'errors': e.args[0]})#, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    source = source.lower()
 
+    if (source not in source2paragraphs):
+        raise Exception(
+            f'Posts from `{source}` cannot be parsed yet. '
+            f'Please choose one of: {", ".join(list(source2paragraphs.keys()))}'
+        )
+    paragraphs = source2paragraphs[source](url)
+    return Response({ 'paragraphs': paragraphs })
 
 @api_view(['POST'])
+@try_except
 def getTranslations(request: HttpRequest):
-
-    try:
-        body = request.data 
-        sentences = body['sentences']
-
-        # translate sentence-by-sentence to reduce mem requirement
-        translations = [
-            nl2en.translate([sentence])
-            for sentence in sentences
-        ]
-        res = { 'translations': translations }
-        return Response(res)
-
-    except exceptions.APIException as e:
-        print(e)
-        return Response(
-            { 'errors': e.detail }, 
-            status=e.status_code
-        )    
-
-    except Exception as e:
-        print(e)
-        # (setting it to default 200, so that that the 
-        # custom error msg can be properly intercepted in frontend)
-        return Response({ 'errors': e.args[0] })#, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    body = request.data
+    sentences = body['sentences']
+    
+    # translate sentence-by-sentence to reduce mem requirement
+    translations = [
+        nl2en.translate([sentence])
+        for sentence in sentences
+    ]
+    res = { 'translations': translations }
+    return Response(res)
