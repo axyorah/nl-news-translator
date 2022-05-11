26f80cebebc094aa85982fec25067ae11b9b9e46
axyorah <elvira.bozileva@gmail.com>
2022-05-11 17:56:01 +0200

    mod: increased the lifetime of access token to 5 days

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,7 +56,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
