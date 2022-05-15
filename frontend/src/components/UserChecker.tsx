71d950273c81ae023655a865116323539cf3eb6a
axyorah <elvira.bozileva@gmail.com>
2022-05-16 00:43:53 +0200

    add: added a component that checks for `userLoginInfo.userDetail` and if it's not found - redirects to login page. This will be used for components/screens that can only be used by authenticated users (e.g., create or view notes)

---
+++ frontend/src/components/UserChecker.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import Loader from './Loader';
+import Message from './Message';
+
+import { StoreState } from '../types/storeTypes';
+import { UserLoginInfo } from '../types/userTypes';
+
+interface UserCheckerState {
+    userLoginInfo: UserLoginInfo
+}
+
+const UserChecker = (props: UserCheckerState): JSX.Element => {
+
+    const { userLoginInfo } = props;
+    const { loading: loadingUserInfo, errors: errorsUserInfo, userDetail } = userLoginInfo || {};
+
+    const history = useHistory();
+
+    // redirect to login page if anonymous user
+    useEffect(() => {
+        if (!userDetail) {
+            history.push('/login');
+        }
+    }, [ history, userDetail]);
+
+    return (
+        <div>
+            { loadingUserInfo ? <Loader /> : null }
+            { errorsUserInfo ? <Message variant='danger'>{errorsUserInfo}</Message> : null }
+        </div>
+    );
+};
+
+const mapStateToProps = (state: StoreState): UserCheckerState => {
+    return {
+        userLoginInfo: state.userLoginInfo
+    };
+};
+
+export default connect<UserCheckerState, {}, {}, StoreState>(
+    mapStateToProps, 
+    {}
+)(UserChecker);
